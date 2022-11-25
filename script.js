f187266cffdeefca5f438ba886049db534e829ae
Walter Wojnarowski <walterwojnarowski@hotmail.com>
2022-11-25 17:25:46 +0000

    Displaying pokemon works, but not searching

---
+++ script.js
@@ -0,0 +1,41 @@
+import pokemonArray from "./data/pokemon.js";
+
+const card = document.querySelector(".card-container");
+const inputBox = document.querySelector(".input");
+
+const generateCards = (pokemonArray) => {
+  pokemonArray.forEach((pokemon) => {
+    return (card.innerHTML += `<div class=card><img src="${
+      pokemon.sprite
+    }" class="card__image"/><div class="card__content"><h2 class="card__heading">${
+      pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)
+    }</h2>
+        <p class="card__text">${
+          pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)
+        } (#${pokemon.id}) is a ${pokemon.types.join(
+      " & "
+    )} type pokemon.</p></div> `);
+  });
+};
+generateCards(pokemonArray);
+
+const filterBySearchTerm = (pokemonArray, searchTerm) => {
+  const filtered = pokemonArray.filter((pokemon) => {
+    return pokemon.includes(searchTerm);
+  });
+
+  return filtered;
+};
+// };
+// console.log(filterBySearchTerm(pokemonArray.name, "z"));
+inputBox.addEventListener("input", (event) => {
+  const filterBySearch = event.target.value;
+  let filteredPokemon = filterBySearchTerm(pokemonArray, filterBySearch);
+  galleryHTML.innerHTML = generateCards(filteredPokemon).join("");
+});
+
+// filterBySearchTerm.addEventListener("input", (event) => {
+//   const filterBySearch = event.target.value;
+//   let filteredPokemon = filterBySearchTerm(pokemon, filterBySearch);
+//   galleryHTML.innerHTML = generateCards(filteredPokemon).join("");
+// });
